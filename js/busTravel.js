2200e48e7d1774143d29680d0fc39ae4dc44152b
Londeka Zikalala <londekazikalala@gmail.com>
2023-05-31 10:44:48 +0200

    First commit

---
+++ js/busTravel.js
@@ -0,0 +1,11 @@
+/*
+create an instance busTravel() with: 
+
+*Create global variables : travel location and cost object; 
+create a function to set points, 
+create a function to set a travel location
+create a function to pick a return trip 
+create a function to calculte the travel cost
+create a function to get the travel cost 
+
+ */
